7b2fa66f079adafe78e53c59e29a5d9febc6afdf
mstars_lab2_06 <mstars_lab2_06@mstars-lab2-06s-iMac.local>
2022-04-13 14:57:38 +0800

    update

--- Ex05.js
+++ Ex05.js
@@ -1,9 +1,9 @@
 function sum(a, b) {
-  if (a + b < 80) {
-    console.log(parseInt(65));
-  } else console.log(parseInt(80));
+  if (a + b < 80 && a + b > 50) {
+    console.log(65);
+  } else console.log(80);
 }
 
-sum(30, 20);
+sum(30, 30);
 sum(50, 60);
 sum(20, 20);
