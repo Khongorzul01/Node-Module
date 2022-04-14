4ffa687330d168d22f0c9b2a325f6fc0ecb1dfb1
mstars_lab2_06 <mstars_lab2_06@mstars-lab2-06s-iMac.local>
2022-04-14 14:48:01 +0800

    ex5

--- day3/Ex-05.js
+++ day3/Ex-05.js
@@ -1,8 +1,36 @@
 var http = require("http");
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+  "Access-Control-Max-Age": 2592000,
+  "Content-Type": "text/html; charset=utf-8",
+};
+
 http
   .createServer(function (request, response) {
-    response.writeHead(200);
-    response.write("<h1> Hello Node!!!</h1>\n");
+    response.writeHead(200, headers);
+    response.write(` <table className='table'>
+    <tbody>
+      <tr>
+        <td>Нэр</td>
+        <td>Нас</td>
+        <td>Хүйс</td>
+        <td>Статус</td>
+      </tr>
+      <tr>
+        <td>Хатнаа</td>
+        <td>20</td>
+        <td>Эр</td>
+        <td>Оюутан</td>
+      </tr>
+      <tr>
+        <td>Ээнээ</td>
+        <td>25</td>
+        <td>Эм</td>
+        <td>Оюутан</td>
+      </tr>
+    </tbody>
+  </table>`);
     response.end();
   })
   .listen(3001);
